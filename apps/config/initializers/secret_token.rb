0aa3b239332e4c631003396a87b704b1f8156539
$ rails new railsinstaller_demo <Admin@desktop1.NAMV.com>
2013-03-19 12:34:08 +0530

    added apps repository

---
+++ apps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apps::Application.config.secret_token = '416ff9f837fec151a2950e4499c8275716f18aeed22e09137bf4670161f47f77f0203baa1bfdf8247f5378be15c629ece61838a12230b55f5f00ee486a6930f9'
